368009f00a7fabcca11840db2fdb3bf53f96476e
Khalidah Aminah <khalidah@gmail.com>
2026-03-30 20:28:59 +0700

    Add integer operations examples and employee profile generator script

---
+++ Python Course/integers_n_floats.py
@@ -0,0 +1,28 @@
+#with integers and floats we can store numeric data types in python
+#we can also perform mathematical operrations
+
+#integers are whole numbers (without decimal points but can  be positive or negative)
+my_int_1 = 56
+my_int_2 = -4
+
+print(type(my_int_1)) # <class 'int'>
+print(type(my_int_2)) # <class 'int'>
+
+#how to perform addition with integers
+my_int_1 = 56
+my_int_2 = 12
+
+#addition
+sum_ints = my_int_1 + my_int_2
+print('Integer Addition: ', sum_ints) #Integer addition: 68
+
+#how to perform subtraction with integers
+
+my_int_1 = 56
+my_int_2 = 12
+
+# Subtraction
+diff_ints = my_int_1 - my_int_2
+print('Integer Subtraction:', diff_ints) # Integer Subtraction: 44
+
+#how to perform multiplication with integers
